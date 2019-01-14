304d2f01607dae86248ad480dfaea08445fa0296
denver-blake <denver.blake26@gmail.com>
2019-01-14 13:59:33 -0500

    working on pilgrim & preacher

--- javascript/castle.js
+++ javascript/castle.js
@@ -1,6 +1,29 @@
 import * as util from "./util.js"
 import {SPECS} from 'battlecode';
 
+function jobsInit(r) {
+    r.jobs = {};
+    r.currentJob = [0,0,0];
+}
+
+function jobsStep(r) {
+    let visible = r.getVisibleRobots();
+
+    for(let i=0;i<visible.length;i++) {
+        if(visible[i].castle_talk === 255) {
+            r.log('Assigning job ' + r.currentJob + ' to id ' + visible[i].id);
+            r.jobs[visible[i].id] = r.currentJob;
+            r.log(r.jobs);
+            break;
+        }
+    }
+}
+
+function createUnit(r, unit, job) {
+    r.currentJob = job
+    return build(r, unit);
+}
+
 function build(r, unit) {
     let dir_coord = [[-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1]];
     let pass_map = r.map;
@@ -22,6 +45,9 @@ function castle_pilgrim_init(r) {
     r.karboniteCoords = util.karboniteCoords(r.map, r.karbonite_map, [r.me.x, r.me.y], util.getMoves(2));
     r.enemyKarboniteCoords = util.karboniteCoords(r.map, r.karbonite_map, [r.enemy_castle[0], r.enemy_castle[1]], util.getMoves(2));
 
+    r.fuelCoords = util.karboniteCoords(r.map, r.fuel_map, [r.me.x, r.me.y], util.getMoves(2));
+    r.fuelIndex = 0;
+
     r.enemy_karb_index = 0;
     while((r.enemyKarboniteCoords[0][0] - r.enemy_castle.x) ** 2 + (r.enemyKarboniteCoords[0][1] - r.enemy_castle.y) ** 2 < 36)
         r.enemy_karb_index++;
@@ -32,48 +58,38 @@ function castle_pilgrim_init(r) {
     r.initial_pilgrim_count = 0;
     r.castleCount = r.getVisibleRobots().length;
     r.stepsSinceLastDefense = 0;
+
+
 }
 
 function castle_pilgrim_step(r) {
     let signaledThisTurn = false;
 
     let target = r.karboniteCoords[r.initial_pilgrim_count];
-    if(target === undefined)
-        r.log("target undef");
-    if(!r.initial_pilgrim_complete) {
-        if(r.getVisibleRobots().length - r.castleCount === 4) {
-            r.initial_pilgrim_complete = true;
-        } else {
-            r.signal(util.signalCoords(target[0], target[1], 0), 2);
-            r.initial_pilgrim_count++;
-            return build(r, SPECS.PILGRIM);
+
+    if(target !== undefined) {
+        if (!r.initial_pilgrim_complete) {
+            if (r.getVisibleRobots().length - r.castleCount >= 4) {
+                r.initial_pilgrim_complete = true;
+            } else {
+                r.signal(util.signalCoords(target[0], target[1], util.PILGRIM_JOBS.MINE_KARBONITE), 2);
+                r.initial_pilgrim_count++;
+                return createUnit(r, SPECS.PILGRIM, [target[0], target[1], util.PILGRIM_JOBS.MINE_KARBONITE]);
+            }
+        }
+
+        let visible = r.getVisibleRobots();
+        for (let i = 0; i < visible.length; i++) {
+            let robot = visible[i];
+
+            if (robot.unit === SPECS.PILGRIM && robot.signal === 1) {
+                r.signal(util.signalCoords(target[0], target[1], util.PILGRIM_JOBS.MINE_KARBONITE), 2);
+                r.jobs[robot.id] = [target[0], target[1], util.PILGRIM_JOBS.MINE_KARBONITE];
+                r.signaledThisTurn = true;
+                r.initial_pilgrim_count++;
+            }
         }
     }
-
-    let visible = r.getVisibleRobots();
-    for(let i=0;i<visible.length;i++) {
-        let robot = visible[i];
-
-        if(robot.unit === SPECS.PILGRIM && robot.signal === 1) {
-            r.signal(util.signalCoords(target[0], target[1], 0), 2);
-            r.signaledThisTurn = true;
-            r.initial_pilgrim_count++;
-        }
-    }
-
-    // if(r.initial_pilgrim_complete && !signaledThisTurn) {
-    //     if(r.bully_preachers_built < 5)
-    //     {
-    //         if(r.karbonite >= 90) {
-    //             let goal_pos = r.enemyKarboniteCoords[r.enemy_karb_index];
-    //             r.signaledThisTurn = true;
-    //             r.signal(util.signalCoords(goal_pos[0], goal_pos[1], 3), 2);
-    //             r.enemy_karb_index++;
-    //             r.bully_preachers_built++;
-    //             return build(r, SPECS.PREACHER);
-    //         }
-    //     }
-    // }
 }
 
 function defenseInit(r) {
@@ -121,47 +137,55 @@ function castlePostRushInit(r) {
     r.preacherTotal = 0;
     r.prophetTotal = 0;
     r.enemyKarboniteIndex = 0;
-
+    r.postRushTotal = 0;
+    r.count = 0;
 }
 function castlePostRushStep(r) {
     let robotMap = r.getVisibleRobotMap();
+    if(r.count >= 2)
+        return;
+    if(r.postRushTotal % 5 === 0 && r.karbonite >= 20 && r.fuel >= 50 && r.fuelIndex < r.fuelCoords.length) {
+        r.postRushTotal++;
+        r.log("Building Fuel Pilgrim");
+        let target = r.fuelCoords[r.fuelIndex];
+        r.fuelIndex++;
+        r.signal(util.signalCoords(target[0], target[1], util.PILGRIM_JOBS.MINE_FUEL), 2);
+        return createUnit(r, SPECS.PILGRIM, [target[0], target[1], util.PILGRIM_JOBS.MINE_FUEL]);
+    }
 
     if (r.currentBuild == SPECS.PREACHER && r.karbonite >= 30 && r.fuel >= 50) {
-        for (let i = 0;i < 8;i++) {
-            let dir = util.directions(i);
-            let newX = r.me.x + dir[0];
-            let newY = r.me.y + dir[1];
-            if (util.withInMap(newX,newY,r) && r.map[newY][newX] && robotMap[newY][newX] == 0) {
-                r.currentBuild = SPECS.PROPHET;
-                let target = r.enemyKarboniteCoords[r.enemyKarboniteIndex];
-                r.enemyKarboniteIndex++;
-                r.log("Builted Pillage Preacher");
-                r.signal(util.signalCoords(target[0], target[1], 4),2);
-                return r.buildUnit(SPECS.PREACHER, dir[0], dir[1]);
-            }
-        }
+        r.count++;
+        r.postRushTotal++;
+
+        r.currentBuild = SPECS.PROPHET;
+        let target = r.enemyKarboniteCoords[r.enemyKarboniteIndex % r.enemyKarboniteCoords.length];
+        r.enemyKarboniteIndex++;
+        r.log("Builted Pillage Preacher");
+        r.signal(util.signalCoords(target[0], target[1], 4),2);
+        return createUnit(r, SPECS.PREACHER, [target[0], target[1], util.PREACHER_JOBS.PILLAGE_KARBONITE]);
+
     }
     if (r.currentBuild == SPECS.PROPHET && r.karbonite >= 25 && r.fuel >= 50) {
-        for (let i = 0;i < 8;i++) {
-            let dir = util.directions(i);
-            let newX = r.me.x + dir[0];
-            let newY = r.me.y + dir[1];
-            if (util.withInMap(newX,newY,r) && r.map[newY][newX] && robotMap[newY][newX] == 0) {
-                r.currentBuild = SPECS.PREACHER;
-                let target = r.enemyKarboniteCoords[r.enemyKarboniteIndex];
+        r.count++;
+        r.postRushTotal++;
 
-                r.log("Builted Pillage Prophet");
-                r.signal(util.signalCoords(target[0], target[1], 4),2);
-                return r.buildUnit(SPECS.PROPHET, dir[0], dir[1]);
-            }
-        }
+        r.currentBuild = SPECS.PREACHER;
+        let target = r.enemyKarboniteCoords[r.enemyKarboniteIndex % r.enemyKarboniteCoords.length];
+
+        r.log("Builted Pillage Prophet");
+        r.signal(util.signalCoords(target[0], target[1], 4),2);
+        return createUnit(r, SPECS.PROPHET, [target[0], target[1], util.PROPHET_JOBS.PILLAGE_KARBONITE]);
     }
 }
 export function castle_step(r) {
     if(r.step === 0) {
+        jobsInit(r);
         defenseInit(r);
         castle_pilgrim_init(r);
-    } else if (r.step === 50) {
+    }
+    jobsStep(r);
+
+    if (r.step === 50) {
         castlePostRushInit(r);
 
     } else if (r.step > 50) {
--- javascript/pilgrim.js
+++ javascript/pilgrim.js
@@ -2,18 +2,19 @@ import * as util from "./util.js"
 import {SPECS} from 'battlecode';
 
 var MODE = {
-  PATH_TO_RESOURCE: 0,
-  MINE: 1,
-  PATH_TO_CASTLE: 2
+    PATH_TO_RESOURCE: 0,
+    MINE: 1,
+    PATH_TO_CASTLE: 2
 };
 
 export function pilgrim_step(r) {
     let visible = r.getVisibleRobots();
 
-    if(r.step == 0) {
+    if(r.step === 0) {
+        r.castleTalk(255);
         r.parent_castle = 0;
         for(let i=0;i<visible.length;i++) {
-            if(visible[i].unit == SPECS.CASTLE) {
+            if(visible[i].unit === SPECS.CASTLE) {
                 r.parent_castle = visible[i];
             }
         }
@@ -21,11 +22,38 @@ export function pilgrim_step(r) {
         r.start = [r.me.x, r.me.y];
         r.castleOffset = [r.me.x - r.parent_castle.x, r.me.y - r.parent_castle.y];
         r.goal = util.decodeCoords(r.parent_castle.signal);
+        r.fuel_job = (r.goal[2] === util.PILGRIM_JOBS.MINE_FUEL);
 
         r.pathMapToKarb = util.pathfindingMap(r.map, r.goal, util.getMoves(2), r);
         r.pathMapToStart = util.pathfindingMap(r.map, r.start, util.getMoves(2), r)
 
         r.mode = MODE.PATH_TO_RESOURCE;
+    }
+
+    if(r.fuel < 4 && r.fuel_job)
+        return;
+    if(r.fuel < 150 && !r.fuel_job)
+        return;
+
+    let rmap = r.getVisibleRobotMap();
+    for(let i=0;i<visible.length;i++) {
+        if(visible[i].team !== r.me.team && (visible[i].unit === SPECS.CRUSADER || visible[i].unit === SPECS.PROPHET || visible[i].unit === SPECS.PREACHER)) {
+            r.castleTalk(3);
+
+            let direction = util.directionTo(r.me.x, r.me.y, visible[i].x, visible[i].y, r);
+            if(direction === undefined)
+                r.log(r.me.x + " " + r.me.y + " " + visible[i].x + " " + visible[i].y);
+
+            let newX = r.me.x - direction[0];
+            let newY = r.me.y - direction[1];
+
+            if(util.withInMap(newX, newY, r) && r.map[newY][newX] === true && rmap[newY][newX] === 0) {
+                //r.log(newX + " " + newY);
+                return r.move(-direction[0], -direction[1]);
+            }
+
+            return util.fuzzy_move(r, -direction[0], -direction[1], 3);
+        }
     }
 
     if(r.mode === MODE.PATH_TO_RESOURCE) {
@@ -45,6 +73,9 @@ export function pilgrim_step(r) {
                     }
                 }
 
+                if(r.pathMapToKarb[r.me.y][r.me.x] === 0) {
+                    r.log(r.me.x + " " + r.me.y);
+                }
 
                 let dx = r.pathMapToKarb[r.me.y][r.me.x][0];
                 let dy = r.pathMapToKarb[r.me.y][r.me.x][1];
@@ -52,27 +83,33 @@ export function pilgrim_step(r) {
                 let newX = r.me.x - dx;
                 let newY = r.me.y - dy;
 
-                if (rmap[newY][newX] === 0) {
+                if (util.withInMap(newX, newY, r) && rmap[newY][newX] === 0) {
                     return r.move(-dx, -dy);
                 }
 
-                return util.fuzzy_move(r, -dx, -dy,3);
+                return util.fuzzy_move(r, -dx, -dy, 3);
 
             } else {
-                r.mode = MODE.MINE;
+                if(r.me.karbonite >= 18 && !r.fuel_job) {
+                    r.mode = MODE.PATH_TO_CASTLE;
+                }
+                if(r.me.fuel >= 90 && r.fuel_job)
+                    r.mode = MODE.PATH_TO_CASTLE;
+
+                return r.mine();
             }
         }
-    } else if(r.mode === MODE.MINE) {
-
-        if(r.me.karbonite >= 18) {
-            r.mode = MODE.PATH_TO_CASTLE;
-        }
-
-        return r.mine();
-
     } else if(r.mode === MODE.PATH_TO_CASTLE) {
 
-        if (r.me.x !== r.start[0] || r.me.y !== r.start[1]) {
+        let castleOffset = null;
+        let visible = r.getVisibleRobots();
+        for(let i=0;i<visible.length;i++) {
+            if(visible[i].team === r.me.team && visible[i].unit === SPECS.CASTLE && (r.me.x - visible[i].x) ** 2 + (r.me.y - visible[i].y) ** 2 <= 2) {
+                castleOffset = [visible[i].x - r.me.x, visible[i].y - r.me.y];
+            }
+        }
+
+        if (castleOffset === null) {
             let rmap = r.getVisibleRobotMap();
 
             let dx = r.pathMapToStart[r.me.y][r.me.x][0];
@@ -81,11 +118,11 @@ export function pilgrim_step(r) {
             let newX = r.me.x - dx;
             let newY = r.me.y - dy;
 
-            if(rmap[newY][newX] === 0) {
+            if(util.withInMap(newX, newY, r) && rmap[newY][newX] === 0) {
                 return r.move(-dx, -dy);
             }
 
-            return util.fuzzy_move(r, -dx, -dy,3);
+            return util.fuzzy_move(r, -dx, -dy, 3);
 
         } else {
             // at goal
@@ -94,7 +131,12 @@ export function pilgrim_step(r) {
             if(r.needs_new_assignment)
                 r.signal(1, 2);
 
-            return r.give(-r.castleOffset[0], -r.castleOffset[1], r.me.karbonite, 0);
+            if(r.fuel_job)
+                r.log("giving " + r.me.fuel + " fuel");
+            else
+                r.log("giving " + r.me.karbonite + " karbonite");
+
+            return r.give(castleOffset[0], castleOffset[1], r.me.karbonite, r.me.fuel);
         }
     }
 }
